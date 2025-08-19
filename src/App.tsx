e73ac44676855f53c40fcf7c7633813e038aca93
Manar Eyad <108298667+ManarEyad7@users.noreply.github.com>
2025-08-19 20:31:02 +0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="App">
-      <h1>Welcome to My Video Surveillance Page</h1>
-      <p>Here you can upload and process your videos.</p>
-    </div>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Welcome to My Video Surveillance Page
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
